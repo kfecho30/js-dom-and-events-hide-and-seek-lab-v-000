89b8b4e53626349198c364d3c228e3c5e410f052
Kelsey Fecho <kfecho30@gmail.com>
2018-06-21 19:38:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function getFirstSelector(selector) {
 function nestedTarget(){
   return document.querySelector('#nested .target')
 }
+
+function increaseRankBy(n){
+  let lis = document.querySelectorAll('ul.ranked-list li')
+  for(const li in lis){
+    li.innerHTML = (li.innerHTML.parseInt() + 1)
+  }
+}
