ed272baa79fda43e36c10f69e504757a4e0518e6
Kelsey Fecho <kfecho30@gmail.com>
2018-06-21 19:35:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function getFirstSelector(selector) {
   return document.querySelector(selector)
 }
 
-function nestedTarget(target, nest){
-  return document.querySelector(`#${nest}.${target}`)
+function nestedTarget(){
+  return document.querySelector(''#nested .target')
 }
